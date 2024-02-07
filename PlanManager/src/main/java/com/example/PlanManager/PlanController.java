9a1b6a935a4dbab39604bff55f3df2e085403ea6
Giuliano Pascarelli <giulianopascarelli@gmail.com>
2024-02-07 17:50:34 -0300

    Added and tested Post endopoint

--- PlanManager/src/main/java/com/example/PlanManager/PlanController.java
+++ PlanManager/src/main/java/com/example/PlanManager/PlanController.java
@@ -3,8 +3,13 @@ package com.example.PlanManager;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 import java.util.Optional;
 
 @RestController
@@ -13,8 +18,8 @@ public class PlanController {
 
     private final PlanRepository planRepository;
 
-    private PlanController(PlanRepository planRepository){
-        this.planRepository=planRepository;
+    private PlanController(PlanRepository planRepository) {
+        this.planRepository = planRepository;
     }
 
     @GetMapping("/{requestedId}")
@@ -25,5 +30,15 @@ public class PlanController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @PostMapping
+    private ResponseEntity<Void> createPlan(@RequestBody Plan newPlan, UriComponentsBuilder ucb) {
+        Plan savedPlan = planRepository.save(newPlan);
+        URI locationOfNewCashCard = ucb
+                .path("plans/{id}")
+                .buildAndExpand(savedPlan.id())
+                .toUri();
+        return ResponseEntity.created(locationOfNewCashCard).build();
     }
 }
--- PlanManager/src/test/java/com/example/PlanManager/PlanManagerApplicationTests.java
+++ PlanManager/src/test/java/com/example/PlanManager/PlanManagerApplicationTests.java
@@ -11,6 +11,8 @@ import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.net.URI;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PlanManagerApplicationTests {
 	@Autowired
@@ -30,10 +32,29 @@ class PlanManagerApplicationTests {
 	}
 
 	@Test
-	void ShouldNotReturnAPlanWhithAnUnknownId(){
+	void ShouldNotReturnAPlanWhithAnUnknownId() {
 		ResponseEntity<String> response = restTemplate.getForEntity("/plans/1000", String.class);
-		
+
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 		assertThat(response.getBody()).isBlank();
 	}
+
+	@Test
+	void shouldCreateANewPlan() {
+		Plan newPlan = new Plan(null, "Second Plan");
+		ResponseEntity<Void> createResponse = restTemplate.postForEntity("/plans", newPlan, Void.class);
+		assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+		URI locationOfNewPlan = createResponse.getHeaders().getLocation();
+		ResponseEntity<String> getResponse = restTemplate.getForEntity(locationOfNewPlan, String.class);
+		assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+		DocumentContext documentContext = JsonPath.parse(getResponse.getBody());
+		Number id = documentContext.read("$.id");
+		String name = documentContext.read("$.name");
+
+		assertThat(id).isNotNull();
+		assertThat(name).isEqualTo("Second Plan");
+
+	}
+
 }
