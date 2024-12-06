0704c9931ea1e08de9f6241518025a004c29abcf
Lelox028 <giulianopascarelli@gmail.com>
2024-12-06 15:29:56 -0300

    GetUniversidadById done

--- StudyPlanManagerApi/src/main/java/com/lelox028/StudyPlanManagerApi/UniversidadController.java
+++ StudyPlanManagerApi/src/main/java/com/lelox028/StudyPlanManagerApi/UniversidadController.java
@@ -3,7 +3,9 @@ package com.lelox028.StudyPlanManagerApi;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,4 +19,13 @@ public class UniversidadController {
         return universidadService.getAllUniversidades();
     }
     
+    @GetMapping("/universidad/{id}")
+    public ResponseEntity<Universidad> getUniversidadById(@PathVariable int id){
+        try {
+            Universidad universidad = universidadService.getUniversidadById(id);
+            return ResponseEntity.ok(universidad);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
--- StudyPlanManagerApi/src/main/java/com/lelox028/StudyPlanManagerApi/UniversidadService.java
+++ StudyPlanManagerApi/src/main/java/com/lelox028/StudyPlanManagerApi/UniversidadService.java
@@ -1,6 +1,7 @@
 package com.lelox028.StudyPlanManagerApi;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,4 +16,13 @@ public class UniversidadService {
         return universidadRepository.findAll();
     }
     
+    public Universidad getUniversidadById(int id){
+        Optional<Universidad> optionalUniversidad = universidadRepository.findById(id);
+        if (optionalUniversidad.isPresent()){
+            return optionalUniversidad.get();
+        }
+        else{
+            throw new RuntimeException("Cannot get item with ID: "+ id);
+        }
+    }
 }
